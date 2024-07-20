b0f76af51a35fec6e926ebbbab89c782aae86b70
Alexander Kosorukov <alex.kosorukov@gmail.com>
2024-07-20 17:22:32 -0400

    Commit project: AngularDotNetAPI-ServerSideCode,  Date: 2024-07-20, 17:22

---
+++ WebAPIWithEF/Controllers/CustomersController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Web;
+using WebAPIWithEF.Models;
+
+namespace WebAPIWithEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        private string serializedObject = "";
+
+        public CustomersController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("CustomerList")]
+        public async Task<JsonResult> GetCustomerList()
+        {
+            List<Customer> customerRawList = await _context.Customers.ToListAsync();
+            List<Customer> customerList = customerRawList.OrderBy(x => x.CustomerId).ToList();
+
+            if (customerList.Count > 0)
+            {
+                serializedObject = JsonConvert.SerializeObject(customerList);
+                return new JsonResult(serializedObject);
+            }
+            else
+            {
+                serializedObject = JsonConvert.SerializeObject(new { ErrorMessage = "No data found." });
+                return new JsonResult(serializedObject);
+            }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetCustomer()
+        {
+            {
+                var parsed = HttpUtility.ParseQueryString(HttpContext.Request.QueryString.ToString());
+                if (parsed["customerId"] == null)
+                {
+                    serializedObject = JsonConvert.SerializeObject(new { ErrorMessage = "The key 'CustomerId' does not exist." });
+                    return new JsonResult(serializedObject);
+                }
+                int customerId = Convert.ToInt32(parsed["customerId"]);
+
+                Customer? customer = await _context.Customers.FirstOrDefaultAsync(el => el.CustomerId == customerId);
+
+                if (customer != null)
+                {
+                    serializedObject = JsonConvert.SerializeObject(customer);
+                    return new JsonResult(serializedObject);
+                }
+                else
+                {
+                    serializedObject = JsonConvert.SerializeObject(new { ErrorMessage = "No data found." });
+                    return new JsonResult(serializedObject);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> PostCustomer()
+        {
+            string? customerStringified = HttpContext.Request.Headers["customer"];
+            if (customerStringified == null)
+            {
+                return new JsonResult("Cannot insert the record: " +
+                  "Http Header's object is null.");
+            }
+            JToken token = JObject.Parse(customerStringified);
+            Customer customer = new Customer();
+            customer = SetCustomer(customer, token);
+
+            try
+            {
+                await _context.Customers.AddAsync(customer);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string? message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message;
+                return new JsonResult("Cannot insert the record with ID=" + customer.CustomerId +
+                    ". Error: " + message + ".");
+            }
+
+            return new JsonResult("The record with CustomerID=" + customer.CustomerId + " inserted!");
+        }
+
+        [HttpPut]
+        public async Task<JsonResult> UpdateCustomer()
+        {
+            string? customerStringified = HttpContext.Request.Headers["customer"];
+            if (customerStringified == null)
+            {
+                return new JsonResult("Cannot update the record: " +
+                  "Http Header's object is null.");
+            }
+            JToken token = JObject.Parse(customerStringified);
+            int customerId = Convert.ToInt32(token.SelectToken("customerId"));
+            Customer? customer = null;
+
+            try
+            {
+                customer = await _context.Customers.FirstOrDefaultAsync(el => el.CustomerId == customerId);
+                if (customer == null)
+                {
+                    return new JsonResult("Cannot update the record with ID= " +
+                        token.SelectToken("customerId") + ". The specified record does not exist.");
+                }
+                customer = SetCustomer(customer, token);
+                await _context.SaveChangesAsync();
+            }
+
+            catch (Exception ex)
+            {
+                string? message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message;
+                return new JsonResult("Cannot update the record with ID=" + customerId +
+                    ". Error: " + message + ".");
+            }
+            return new JsonResult("The record with CustomerID=" + customerId + " updated!"); ;
+        }
+
+        [HttpDelete]
+        public async Task<JsonResult> DeleteCustomer()
+        {
+            var parsed = HttpUtility.ParseQueryString(HttpContext.Request.QueryString.ToString());
+            int customerId = Convert.ToInt32(parsed["CustomerId"]);
+
+            try
+            {
+                int rowsDeleted = await _context.Customers.Where(t => t.CustomerId == customerId).ExecuteDeleteAsync();
+                if (rowsDeleted == 0)
+                {
+                    return new JsonResult("Cannot delete the record with ID= " +
+                        customerId + ". The specified record does not exist.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                string? message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message;
+                return new JsonResult("Cannot delete the record with ID=" +
+                    customerId +
+                    ". Error: " + message + ".");
+            }
+            return new JsonResult("The record with CustomerID=" + customerId + " deleted!");
+        }
+
+        private Customer SetCustomer(Customer customer, JToken token)
+        {
+            customer.CustomerId = Convert.ToInt32(token.SelectToken("customerId"));
+            customer.CustomerName = Convert.ToString(token.SelectToken("customerName"));
+            customer.CreatedDate = DateOnly.Parse(Convert.ToString(token.SelectToken("createdDate")));
+            customer.CustomerTypeId = Convert.ToString(token.SelectToken("customerTypeId"));
+            customer.StateCode = Convert.ToString(token.SelectToken("stateCode"));
+
+            return customer;
+        }
+    }
+}
---
+++ WebAPIWithEF/Models/StoreContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIWithEF.Models;
+
+public partial class StoreContext : DbContext
+{
+    public StoreContext()
+    {
+    }
+
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name = ConnectionStrings:CustomersAppCon");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64B868E5D324");
+
+            entity.Property(e => e.CustomerId)
+                .ValueGeneratedNever()
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.CustomerName).HasMaxLength(100);
+            entity.Property(e => e.CustomerTypeId)
+                .HasMaxLength(20)
+                .HasColumnName("CustomerTypeID");
+            entity.Property(e => e.StateCode).HasMaxLength(2);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
